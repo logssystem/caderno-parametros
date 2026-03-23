f07f0575d68b4d51d9aab173df8f82c8af77edfd
logssystem <joaorodrigue0@gmail.com>
2026-03-23 17:02:09 -0300

    Update resumo.js

--- resumo.js
+++ resumo.js
@@ -459,7 +459,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /* ================= VOLTAR ================= */
 window.voltar = function () {
-  window.location.href = "index.php"; 
+  window.location.href = "index.html"; 
 };
 
 /* =================================================
