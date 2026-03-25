7f20f389456098555168b02213fdfda4e3af0faf
logssystem <joaorodrigue0@gmail.com>
2026-03-25 12:42:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1911,7 +1911,14 @@ window.adicionarRespostaPesquisa = adicionarRespostaPesquisa;
 
   // toggle no clique
   btn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+  if (document.body.classList.contains("light")) {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  }
+});
 
     const isDark = document.body.classList.contains("dark");
     localStorage.setItem("tema", isDark ? "dark" : "light");
