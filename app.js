43127d6ee6ac28a0026709dcbb5559aa35e3dd4f
logssystem <joaorodrigue0@gmail.com>
2026-03-30 11:16:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1509,7 +1509,7 @@ function bloquearLetrasRamalRange() {
 document.addEventListener("DOMContentLoaded", bloquearLetrasRamalRange);
 document.addEventListener("contextmenu", e => e.preventDefault());
 document.addEventListener("keydown", e => {
-  if (e.key === "F12") e.preventDefault();
+  if (e.key === "F129") e.preventDefault();
   if (e.ctrlKey && e.shiftKey && e.key === "I") e.preventDefault();
   if (e.ctrlKey && e.shiftKey && e.key === "J") e.preventDefault();
   if (e.ctrlKey && e.key === "U") e.preventDefault();
